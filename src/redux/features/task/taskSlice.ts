e12df8fa7c6c6842020f30ea1971a0f0cfd2fd17
Iftekar Rahman Ruhit <iftekarrahmanruhit2003@gmail.com>
2025-08-04 15:14:53 +0600

    added task delete functionality

--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -1,41 +1,64 @@
-import type { RootState } from "@/redux/store";          
-import type { ITask } from "../../../types";                
-import { createSlice, nanoid, type PayloadAction } from "@reduxjs/toolkit"; 
+import type { RootState } from "@/redux/store";
+import type { ITask } from "../../../types";
+import { createSlice, nanoid, type PayloadAction } from "@reduxjs/toolkit";
 
 // Defining the shape of the slice state
 interface InitialState {
-  tasks: ITask[];                                           
-  filter: "All" | "High" | "Medium" | "Low";                
+  tasks: ITask[];
+  filter: "All" | "High" | "Medium" | "Low";
 }
 
 // Initial state for the slice
 const initialState: InitialState = {
-  tasks: [],
+  tasks: [
+    {
+      id: "asdfghjklsasfas",
+      title: "Initialize Frontend",
+      description: "Create Home Page and Routing",
+      dueDate: "2025-08-25",
+      isCompleted: false,
+      priority: "high",
+    },
+  ],
   filter: "All",
 };
 
 // Defining the shape of form data (without id and isCompleted)
 type DraftTask = Pick<ITask, "title" | "description" | "dueDate" | "priority">;
 
-
 const createTask = (taskData: DraftTask): ITask => {
   return {
-    id: nanoid(),  // Auto-generate a unique task ID
+    id: nanoid(), // Auto-generate a unique task ID
     isCompleted: false, // New tasks are incomplete by default
-    ...taskData,   // Spread the rest of the form values
+    ...taskData, // Spread the rest of the form values
   };
 };
 
 // Creating the Redux slice
 export const taskSlice = createSlice({
-  name: "task",    
-  initialState,       
+  name: "task",
+  initialState,
   reducers: {
     // Reducer to add a new task to the list
     addTask: (state, action: PayloadAction<DraftTask>) => {
-      const taskData = createTask(action.payload); 
-      state.tasks.push(taskData);                  
+      const taskData = createTask(action.payload);
+      state.tasks.push(taskData);
     },
+
+    toggleCompleteState: (state, action: PayloadAction<string>) => {
+      state.tasks.forEach((task) =>
+        task.id === action.payload
+          ? (task.isCompleted = !task.isCompleted)
+          : task
+      );
+    },
+
+    deleteTask: (state, action: PayloadAction<string>) => {
+     state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    },
+
+
+
   },
 });
 
@@ -50,7 +73,7 @@ export const selectFilter = (state: RootState) => {
 };
 
 // Exporting the action(s) so components can dispatch them
-export const { addTask } = taskSlice.actions;
+export const { addTask,toggleCompleteState,deleteTask } = taskSlice.actions;
 
 // Exporting the reducer so it can be added to the store
 export default taskSlice.reducer;
